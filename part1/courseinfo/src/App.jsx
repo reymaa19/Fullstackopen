baf69ef35340d09a1d029d7756443314d59b9bf1
reymaa19 <rcmaala@gmail.com>
2023-10-20 19:14:36 -0500

    Finish courseinfo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,66 @@
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
+  };
+
+  const Part = (props) => {
+    const { name, exercises } = props.part;
+
+    return (
+      <p>
+        {name} {exercises}
+      </p>
+    );
+  };
+
+  const Content = (props) => {
+    const [first, second, third] = props.parts;
+
+    return (
+      <div>
+        <Part part={first} />
+        <Part part={second} />
+        <Part part={third} />
+      </div>
+    );
+  };
+
+  const Total = (props) => {
+    const [first, second, third] = props.parts;
+
+    return (
+      <p>
+        Number of exercises{" "}
+        {first.exercises + second.exercises + third.exercises}
+      </p>
+    );
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
