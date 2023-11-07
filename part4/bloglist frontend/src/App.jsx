c1a957cfb925591571ebda9080bed8e426777761
reymaa19 <rcmaala@gmail.com>
2023-11-07 01:24:04 -0600

    start part4

---
+++ part4/bloglist frontend/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return <></>
+}
+
+export default App
+
+// In the exercises for this part, we will be building a blog list application,
+// that allows users to save information about interesting blogs they have stumbled
+// across on the internet. For each listed blog we will save the author, title, URL,
+// and amount of upvotes from users of the application.
