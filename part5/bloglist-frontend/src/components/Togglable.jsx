65c494d886960d28ebe942351ab2abed9e710d2a
reymaa19 <rcmaala@gmail.com>
2023-12-13 00:26:20 -0600

    finish exercise 5.12

--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import { forwardRef, useImperativeHandle, useState } from 'react'
 
 const Togglable = forwardRef((props, refs) => {
@@ -26,5 +27,11 @@ const Togglable = forwardRef((props, refs) => {
     </div>
   )
 })
+
+Togglable.displayName = 'Togglable'
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+}
 
 export default Togglable
