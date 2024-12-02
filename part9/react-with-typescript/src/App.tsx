a3f008d56747d5d39633cc5b6a63a2baded77e81
reymaa19 <rcmaala@gmail.com>
2024-12-02 13:22:43 -0600

    finish exercise 9.16

--- part9/react-with-typescript/src/App.tsx
+++ part9/react-with-typescript/src/App.tsx
@@ -2,25 +2,82 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Total from "./components/Total";
 
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+}
+
+interface CoursePartDesc extends CoursePartBase {
+    description: string;
+}
+
+interface CoursePartBasic extends CoursePartDesc {
+    kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartDesc {
+    backgroundMaterial: string;
+    kind: "background";
+}
+
+interface CoursePartSpecial extends CoursePartDesc {
+    requirements: string[];
+    kind: "special";
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
+const courseParts: CoursePart[] = [
+    {
+        name: "Fundamentals",
+        exerciseCount: 10,
+        description: "This is an awesome course part",
+        kind: "basic",
+    },
+    {
+        name: "Using props to pass data",
+        exerciseCount: 7,
+        groupProjectCount: 3,
+        kind: "group",
+    },
+    {
+        name: "Basics of type Narrowing",
+        exerciseCount: 7,
+        description: "How to go from unknown to string",
+        kind: "basic",
+    },
+    {
+        name: "Deeper type usage",
+        exerciseCount: 14,
+        description: "Confusing description",
+        backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+        kind: "background",
+    },
+    {
+        name: "TypeScript in frontend",
+        exerciseCount: 10,
+        description: "a hard part",
+        kind: "basic",
+    },
+    {
+        name: "Backend development",
+        exerciseCount: 21,
+        description: "Typing the backend",
+        requirements: ["nodejs", "jest"],
+        kind: "special",
+    },
+];
+
+const courseName = "Half Stack application development";
+
+const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
+
 const App = () => {
-    const courseName = "Half Stack application development";
-    const courseParts = [
-        {
-            name: "Fundamentals",
-            exerciseCount: 10,
-        },
-        {
-            name: "Using props to pass data",
-            exerciseCount: 7,
-        },
-        {
-            name: "Deeper type usage",
-            exerciseCount: 14,
-        },
-    ];
-
-    const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
-
     return (
         <div>
             <Header courseName={courseName} />
--- part9/react-with-typescript/src/components/Content.tsx
+++ part9/react-with-typescript/src/components/Content.tsx
@@ -1,15 +1,88 @@
-interface CoursePart {
+interface CoursePartBase {
     name: string;
     exerciseCount: number;
 }
+
+interface CoursePartDesc extends CoursePartBase {
+    description: string;
+}
+
+interface CoursePartBasic extends CoursePartDesc {
+    kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartDesc {
+    backgroundMaterial: string;
+    kind: "background";
+}
+
+interface CoursePartSpecial extends CoursePartDesc {
+    requirements: string[];
+    kind: "special";
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
+const assertNever = (value: never): never => {
+    throw new Error(`Unhandled discriminated union member: ${JSON.stringify(value)}`);
+};
+
+const Part = ({ coursePart }: { coursePart: CoursePart }) => {
+    switch (coursePart.kind) {
+        case "basic":
+            return (
+                <div>
+                    <h3>
+                        {coursePart.name} {coursePart.exerciseCount}
+                    </h3>
+                    <p>{coursePart.description}</p>
+                </div>
+            );
+        case "group":
+            return (
+                <div>
+                    <h3>
+                        {coursePart.name} {coursePart.exerciseCount}
+                    </h3>
+                    <p>project exercises {coursePart.groupProjectCount}</p>
+                </div>
+            );
+        case "background":
+            return (
+                <div>
+                    <h3>
+                        {coursePart.name} {coursePart.exerciseCount}
+                    </h3>
+                    <p>{coursePart.description}</p>
+                    <p>{coursePart.backgroundMaterial}</p>
+                </div>
+            );
+        case "special":
+            return (
+                <div>
+                    <h3>
+                        {coursePart.name} {coursePart.exerciseCount}
+                    </h3>
+                    <p>{coursePart.description}</p>
+                    <p>required skills: {coursePart.requirements.join(", ")}</p>
+                </div>
+            );
+        default:
+            assertNever(coursePart);
+            break;
+    }
+};
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
     return (
         <div>
             {courseParts.map((coursePart: CoursePart) => (
-                <p>
-                    {coursePart.name} {coursePart.exerciseCount}
-                </p>
+                <Part coursePart={coursePart} key={coursePart.name} />
             ))}
         </div>
     );
